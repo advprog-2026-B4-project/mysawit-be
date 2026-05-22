2dbe8eb2f7d0d145a3d375e8d726c35b42b1f5df
Vin-Ren <reaxumrx@gmail.com>
2026-05-22 19:12:13 +0700

    fix(observability): remove script mount, use default prometheus entrypoint

--- deploy/observability/prometheus-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-exec /bin/prometheus \
-  --config.file=/etc/prometheus/prometheus.yml \
-  --storage.tsdb.path=/prometheus \
-  --storage.tsdb.retention.time=30d \
-  --web.enable-lifecycle

ce660f19883b1f16a6955a1fe6750919c7c6959d
Vin-Ren <reaxumrx@gmail.com>
2026-05-22 19:13:40 +0700

    fix(observability): bundle entrypoint script in prometheus dir mount, restore substitution

---
+++ deploy/observability/prometheus/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+: "${BACKEND_HOST_MAIN:=mysawit-be-main}"
+: "${BACKEND_HOST_STAGING:=mysawit-be-staging}"
+
+sed \
+  -e "s/BACKEND_HOST_MAIN_PLACEHOLDER/${BACKEND_HOST_MAIN}/g" \
+  -e "s/BACKEND_HOST_STAGING_PLACEHOLDER/${BACKEND_HOST_STAGING}/g" \
+  /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
+
+echo "=== Prometheus config ==="
+cat /tmp/prometheus.yml
+
+exec /bin/prometheus \
+  --config.file=/tmp/prometheus.yml \
+  --storage.tsdb.path=/prometheus \
+  --storage.tsdb.retention.time=30d \
+  --web.enable-lifecycle
